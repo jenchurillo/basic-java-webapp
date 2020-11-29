c3255b3e2acd1381ecd40f8c9c7c587c80c76fb8
jenchurillo <74518300+jenchurillo@users.noreply.github.com>
2020-11-29 15:18:31 -0500

    Create food.java

---
+++ src/main/java/servlet/food.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "food",
+urlPatterns = {"/ihiFhirAssignment_war_exploded/food"})
+public class food extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/food.jsp").forward(request,response);
+
+    }
+}
+
