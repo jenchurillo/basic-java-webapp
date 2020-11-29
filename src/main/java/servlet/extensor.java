5d11193be8c601970e381969c6e46c93f63f8cca
jenchurillo <74518300+jenchurillo@users.noreply.github.com>
2020-11-29 15:14:35 -0500

    Create extensor.java

---
+++ src/main/java/servlet/extensor.java
@@ -0,0 +1,23 @@
+ package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "extensor",
+urlPatterns = {"/ihiFhirAssignment_war_exploded/extensor"})
+public class extensor extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/extensor.jsp").forward(request,response);
+
+    }
+}
+
+ 
