dac93d375878cebd696e6d60fec2f8249e780ade
jenchurillo <74518300+jenchurillo@users.noreply.github.com>
2020-11-29 15:35:41 -0500

    Create stretching.java

---
+++ src/main/java/servlet/stretching.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "stretching",
+urlPatterns = {"/ihiFhirAssignment_war_exploded/stretching"})
+public class stretching extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/stretching.jsp").forward(request,response);
+
+    }
+}
+
