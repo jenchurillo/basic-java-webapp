332bd9edfce96ed47b0670ba9e455bcf6051fc7c
jenchurillo <74518300+jenchurillo@users.noreply.github.com>
2020-11-29 15:11:38 -0500

    Create cycling.java

---
+++ src/main/java/servlet/cycling.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "cycling",
+  urlPatterns = {"/ihiFhirAssignment_war_exploded/cycling"}
+)
+
+public class cycling extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/cycling.jsp").forward(request,response);
+
+    }
+}
+
+
+
+           
+           
